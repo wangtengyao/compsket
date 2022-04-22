4dbd2b2ec8f83a1f36256876ae005d505c35ed24
gaofengnan <gao.fengnan@gmail.com>
2022-04-22 21:52:49 +0800

    minor

--- python/compsket.py
+++ python/compsket.py
@@ -4,9 +4,9 @@ import pandas as pd
 from sklearn import linear_model
 
 """
-Compute the noise standard deviation in a sparse linear regression model with response z and covariates W
+Compute the noise variance in a sparse linear regression model with response z and covariates W
 """
-def dickerNoiseSd(W, z):
+def dickerNoiseVar(W, z):
     m, p = W.shape
     W_Gram = np.matmul(W.T, W) / m
     m1hat = np.sum(np.diagonal(W_Gram)) / p
@@ -41,7 +41,7 @@ def complementarySketching(X1, X2, y1, y2, sparse=True, sigma=None):
     
     # compute sigma, the noise variance, using W and z if necessary
     if sigma is None:
-        sigma = dickerNoiseSd(W, z)
+        sigma = dickerNoiseVar(W, z)
 
     if sigma <= 0:
         return (np.nan, np.nan)
@@ -58,7 +58,7 @@ def complementarySketching(X1, X2, y1, y2, sparse=True, sigma=None):
         Q_thresh = Q * (np.abs(Q) > lam)
         test_stat = np.sum(Q_thresh ** 2)
     else:
-        tau = m + sqrt(8 * np.log(p)) + 4 * np.log(p)
+        tau = m + np.sqrt(8 * np.log(p)) + 4 * np.log(p)
         test_stat = np.sum(z ** 2)
     
     test_result = 1 if test_stat > tau else 0
@@ -94,6 +94,104 @@ def differentialNetworkAnalysis(X1, X2, num_partners=0, nodes=None, sparse=True,
     # results will be stored in df
     if nodes is None: 
         nodes = range(p)
+    nodes = list(nodes)
+    df = pd.DataFrame(index=nodes, columns=['test_stat', 'test_result', 'interacting_partners'])
+    
+    for j in nodes:
+        i = nodes.index(j)
+        if trace: 
+            print('Computing {current}/{total} node...'.format(current=i+1, total=len(nodes)), end='\r')
+            
+        # update the sketching matrix for each node
+        Q1, R1 = sp.linalg.qr_delete(B0, R0, k=j, which='col')
+        u = X[:, j] - np.matmul(Q1, np.matmul(Q1.T, X[:, j]))
+        w = u / np.linalg.norm(u)
+        A = np.concatenate((A0, w.reshape((-1,1))), axis=1)
+        
+        # construct complementarily sketched design W and response z
+        A1 = A[range(n1), :].T
+        A2 = A[range(n1, n), :].T
+        W = np.matmul(A1, np.delete(X1, j, axis=1)) - np.matmul(A2, np.delete(X2, j, axis=1))
+        z = np.matmul(A1, X1[:, j]) + np.matmul(A2, X2[:, j])
+        
+        # compute sigma, the noise variance, using W and z
+        sigma = dickerNoiseVar(W, z)
+        if sigma <= 0:
+            df.iloc[i, :] = [0, 0, '']
+            continue
+        
+        # standardise W and z
+        W = W / sigma; z = z / sigma
+        Wtilde = W / np.sqrt(np.sum(W ** 2, axis=0))
+        
+        # compute test statistics based on sparsity knoweldge
+        if sparse:
+            lam = np.sqrt(4 * np.log(p-1))
+            tau = 3 * np.log(p-1)
+            Q = np.matmul(Wtilde.T, z)
+            Q_thresh = Q * (np.abs(Q) > lam)
+            test_stat = np.sum(Q_thresh ** 2)
+        else:
+            tau = m + np.sqrt(8 * np.log(p)) + 4 * np.log(p)
+            test_stat = np.sum(z ** 2)
+
+        test_result = 1 if test_stat > tau else 0
+        df.iloc[i, :] = [test_stat, test_result, '']
+        
+        # compute interacting partners using Least Angle Regression of z against W
+        if test_result==1 and num_partners>0:
+            other_nodes = np.delete(range(p), j)
+            lars = linear_model.Lars(n_nonzero_coefs=num_partners, fit_intercept=True, normalize=False)
+            lars.fit(W, z)
+            df.iloc[i, 2] = other_nodes[lars.active_]
+        
+    if trace:
+        print()
+        print('Finished: {} significant node found.'.format(np.sum(df['test_result'])))
+        
+    return df
+
+
+"""
+Differential network testing for two covariate matrices X1 (shape n1 x p) and X2 (shape n2 x p)
+This has essentially the same effect as applying complementarySketching() p times comparing 
+nodewise regressions X1[:, j] ~ X1[:, -j] and X2[:, j] ~ X2[:, -j], though code is optimised
+to compute the sketching matrix only once. 
+
+It also can output top num_partners interacting partners of significant nodes found. The nodes
+parameter allows for faster computation if only a subset of nodes is interested. Set trace=True
+to output computational progress.
+
+The difference between this one and the one without Intercept is that it allows an extra intercept term to be added to the design such that centerisation of columns in the design could make sense.
+"""
+
+
+def differentialNetworkAnalysisIntercept(X1, X2, num_partners=0, nodes=None, sparse=True, intercept= True, trace=False):
+    # stack covariates and responses into X and y and get shape parameters
+    X = np.concatenate((X1, X2), axis=0)
+    n, _ = X.shape
+    n1, _ = X1.shape
+    if intercept: # if intercept term is necessary, centre X's columns first, and then add an extra columns of ones
+        X = X - X.sum(axis= 0)/n
+        X = np.concatenate((X, np.ones((n,1))), axis=1)
+    _, p = X.shape
+
+    
+    # precompute a sketching matrix; the sketching matrix for individual nodes can be obtained
+    # by augmenting A0 with a single column
+    if trace: 
+        print('Computing complementary sketches...')
+    Q, R = np.linalg.qr(X, mode='complete')
+    A0 = Q[:, range(p, n)]
+    B0 = Q[:, range(p)]
+    R0 = R[range(p),:]
+    
+    # results will be stored in df
+    if nodes is None:
+        if intercept:
+            nodes = range(p-1)
+        else:
+            nodes = range(p)
     nodes = list(nodes)
     df = pd.DataFrame(index=nodes, columns=['test_stat', 'test_result', 'interacting_partners'])
     
--- python/real_data.py
+++ python/real_data.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn import linear_model
 import sklearn
 import time
+from datetime import datetime
 from compsket import *
 
 
@@ -11,7 +12,7 @@ from compsket import *
 read in data
 """
 print('read in data...')
-dat = pd.read_csv('../data/CD4_TREG_in_thymus.csv.gz', index_col=0)
+dat = pd.read_csv('CD4_TREG_in_thymus.csv.gz', index_col=0)
 X = np.array(dat.iloc[:,1:])
 gene_names = np.array(dat.columns[1:])
 CD4_filter = dat['anno_lvl_2_final_clean'] == 'CD4+T'
@@ -24,24 +25,28 @@ X2 = np.array(X[~CD4_filter, :])
 #X1 = X1 - np.average(X1, axis=0)
 #X2 = X2 - np.average(X2, axis=0)
 t = time.time()
-#nodes = [1, 133, 180]
-nodes = None
-result = differentialNetworkAnalysis(X1, X2, nodes=nodes, num_partners=8, trace=True)
+nodes = [1, 133, 180]
+# nodes = range(100,200)
+# nodes = None
+result = differentialNetworkAnalysisIntercept(X1, X2, nodes=nodes, num_partners=8, intercept=False, trace=True)
+#result = differentialNetworkAnalysis(X1, X2, nodes=nodes, num_partners=8, trace=True)
 print('{} seconds elapsed.'.format(time.time() - t))
 
 """
 save results
 """
-print(result)
-result = result.loc[result['test_result'] == 1, :]
-result.index = gene_names[result.index]
-result['interacting_genes'] = [gene_names[v] for v in result['interacting_partners']]
+# print(result)
+result_identified = result.loc[result['test_result'] == 1, :]
+result_identified.index = np.array(gene_names)[result_identified.index]
+result_identified.insert(len(result_identified.columns), 'interacting_genes', [gene_names[v] for v in result_identified['interacting_partners']])
 
 def DGE(x1, x2, alpha):
     pvals = [sp.stats.mannwhitneyu(x1[:,j], x2[:,j])[1] for j in range(x1.shape[1])]
     return np.where(np.array(pvals) < alpha, 1, 0)
-        
-result['DGE'] = [DGE(X1[:,v], X2[:,v], 0.05 / X1.shape[1]) for v in result['interacting_partners']]
-result.pop('interacting_partners');
-result.to_csv('result.csv', index=True, header=True, sep=',')
+
+result_identified.insert(len(result_identified.columns),'DGE', [DGE(X1[:,v], X2[:,v], 0.05 / X1.shape[1]) for v in result_identified['interacting_partners']])
+result_identified.pop('interacting_partners');
+date = datetime.now().strftime("%Y%m%d-%H%M%S")
+csv_filename = f"result_{date}.csv"
+result_identified.to_csv(csv_filename, index=True, header=True, sep=',')
 
